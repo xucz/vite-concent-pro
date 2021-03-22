54e83f010f82ef31bc20fdf7d1c3d73ed27c5027
dravenwu <dravenwu@tencent.com>
2021-03-22 11:09:14 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -32,28 +32,28 @@ export default defineConfig({
   plugins: [
     // react-refresh插件
     reactRefresh(),
-    // 按需引用的插件
-    vitePluginImp({
-      libList: [
-        {
-          libName: "antd",
-          style: (name) => {
-            const less = fs.existsSync(path.resolve(__dirname, `node_modules/antd/es/${name}/style/index.less`))
-            if (less) {
-              return `antd/es/${name}/style/index.less`;
-            } else {
-              const css = fs.existsSync(path.resolve(__dirname, `node_modules/antd/es/${name}/style/css.js`))
-              if (css) {
-                return `antd/es/${name}/style/css.js`;
-              } else {
-                return false;
-              }
-            }
-          },
-          libDirectory: 'es',
-        },
-      ],
-    })
+    // 按需引用的插件, 因为主题设置不能
+    // vitePluginImp({
+    //   libList: [
+    //     {
+    //       libName: "antd",
+    //       style: (name) => {
+    //         const less = fs.existsSync(path.resolve(__dirname, `node_modules/antd/es/${name}/style/index.less`))
+    //         if (less) {
+    //           return `antd/es/${name}/style/index.less`;
+    //         } else {
+    //           const css = fs.existsSync(path.resolve(__dirname, `node_modules/antd/es/${name}/style/css.js`))
+    //           if (css) {
+    //             return `antd/es/${name}/style/css.js`;
+    //           } else {
+    //             return false;
+    //           }
+    //         }
+    //       },
+    //       libDirectory: 'es',
+    //     },
+    //   ],
+    // })
   ],
   css: {
     preprocessorOptions: {
